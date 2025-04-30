0065d55d6e57566fb73387ff14de99a2f2e21b76
Hrishikesh Shinde <rishikeshs2003@gmail.com>
2025-04-30 16:30:42 +0530

    json-file-imp

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 
 
-const math = require("./math");
+// const math = require("./math");
 
-console.log("sum", math.sum(2,3));
-console.log("multi" , math.multi(2,3));
-console.log("div" ,math.div(2,3));
-console.log("sub" ,math.sub(2,3));
+// console.log("sum", math.sum(2,3));
+// console.log("multi" , math.multi(2,3));
+// console.log("div" ,math.div(2,3));
+// console.log("sub" ,math.sub(2,3));
+
+
+import { sum, multi } from "./math.js" ; 
+
+console.log(sum(5,6));
+console.log(multi(5,6));
